9ff9e8d79c580a88b7941e75661f127a1b42d121
Hélio <insign@gmail.com>
2025-11-28 00:12:51 -0300

    Adicionar suporte prioritário ao speedtest++
    Alterações detalhadas:
    - Adicionado speedtest++ como primeira opção de executável (prioridade máxima)
    - Adaptado parsing da saída JSON do speedtest++ usando jq + awk para converter strings
    - Corrigido cálculo de download/upload de bits/s para Mbps
    - Atualizado cabeçalho do script incluindo speedtest++ nas dependências
    - Versão atualizada de v1.0 para v1.1
    - Mantida compatibilidade com speedtest-cli e speedtest (ookla)
    
    Ordem de prioridade dos executáveis:
    1. speedtest++ (novo)
    2. speedtest-cli (sivel)
    3. speedtest (ookla)

--- speedtest.1h.sh
+++ speedtest.1h.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
-# Bandwith test, using sivel/speedtest-cli or ookla speedtest CLI (best, needs to install jq)
+# Bandwith test, using speedtest++, sivel/speedtest-cli or ookla speedtest CLI (needs jq)
 # Needs xclip to copy to clipboard
-# <bitbar.title>Bandwith test, using speedtest-cli or ookla speedtest CLI</bitbar.title>
-# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.title>Bandwith test, using speedtest++, speedtest-cli or ookla speedtest CLI</bitbar.title>
+# <bitbar.version>v1.1</bitbar.version>
 # <bitbar.author>Hélio</bitbar.author>
 # <bitbar.author.github>insign</bitbar.author.github>
-# <bitbar.desc>Bandwith test, using speedtest-cli or ookla speedtest CLI</bitbar.desc>
+# <bitbar.desc>Bandwith test, using speedtest++, speedtest-cli or ookla speedtest CLI</bitbar.desc>
+# <bitbar.dependencies>speedtest++</bitbar.dependencies>
 # <bitbar.dependencies>speedtest-cli</bitbar.dependencies>
 # <bitbar.dependencies>speedtest</bitbar.dependencies>
 # <bitbar.dependencies>xclip</bitbar.dependencies>
@@ -16,13 +17,33 @@
 # SERVER_ID=10843 # currently works better with ookla speedtest cli
 
 PATH=$PATH:$HOME/.local/bin
+# shellcheck disable=SC2028
+
 
 if [ "$1" = "share" ]; then
         # Copy  to the clipboard
         echo "$2" | xclip -selection clipboard
         $(which notify-send) 'Copied!' 'Sent to clipboard'
 else
-        if [[ -x "$(command -v speedtest-cli)" ]]; then
+        if [[ -x "$(command -v speedtest++)" ]]; then
+                OUTPUT=$($(command -v "speedtest++") --output json ${SERVER_ID:+--test-server=$SERVER_ID})
+                DOWNLOAD=$(jq -r '.download' <<<"$OUTPUT" | awk '{printf "%.2f", $1 / 1e+6}')
+                UPLOAD=$(jq -r '.upload' <<<"$OUTPUT" | awk '{printf "%.2f", $1 / 1e+6}')
+                PING=$(jq -r '.ping' <<<"$OUTPUT")
+                IP=$(jq -r '.client.ip' <<<"$OUTPUT")
+                ISP=$(jq -r '.client.isp' <<<"$OUTPUT")
+                SERVER=$(jq '.server | .name + ", " + .sponsor' -r <<<"$OUTPUT")
+                MAP=$(jq -r '.server | .name + ", " + .sponsor' <<<"$OUTPUT" | jq -sRr @uri)
+
+                echo "${DOWNLOAD%%.*} Mbps | refresh=true"
+                echo '---'
+                echo "$ISP\n$IP | terminal=false bash='$0' param1=share param2=$IP"
+                echo "${DOWNLOAD%%.*} Mbps\t<i>download</i>"
+                echo "\t\t${UPLOAD%%.*} Mbps\t<i>upload</i>"
+                echo "\t\t${PING%%.*} ms\t\t<i>ping</i>"
+                echo "$SERVER | href=https://google.com/maps?q=$MAP"
+                echo "Test again | refresh=true"
+        elif [[ -x "$(command -v speedtest-cli)" ]]; then
                 OUTPUT=$($(command -v "speedtest-cli") --json --share ${SERVER_ID:+--server=$SERVER_ID})
                 DOWNLOAD=$(jq '.download / 1e+6' <<<"$OUTPUT")
                 UPLOAD=$(jq '.upload / 1e+6' <<<"$OUTPUT")
